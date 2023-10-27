93b6a25dfedc6987749df2883042291731c71168
JuicyChann <nik.xeiladakis@gmail.com>
2023-10-27 23:03:00 +0300

    change description

--- yt-anti-adblock-solution.js
+++ yt-anti-adblock-solution.js
@@ -3,8 +3,8 @@
 // @namespace    https://e-z.bio/yaw
 // @homepage     https://github.com/Yaw-Dev/YT-AntiAdBlock-Bypass
 // @icon         https://www.gstatic.com/youtube/img/branding/favicon/favicon_192x192.png
-// @version      1.7.3
-// @description  A simple method of bypassing YouTube's AdBlock Detection using Enhancer for YouTube's "Remove Ads" button. Does not require the use of any external website like similar tools do. | Now featuring a GUI for easy configuration!
+// @version      1.7.4
+// @description  A simple method of bypassing YouTube's AdBlock Detection using Enhancer for YouTube's "Remove Ads" button, featuring a GUI for easy configuration! Does not require the use of any external website like similar tools do.
 // @author       Yaw
 // @match        https://www.youtube.com/*
 // @license      MIT
